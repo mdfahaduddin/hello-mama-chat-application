d5369d19de253468f8429ee5dc80bb0d60545007
Md Fahad Uddin <mdfahaduddin182803@gmail.com>
2021-09-03 12:20:46 +0600

    Setup server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import './App.css';
+
+import {ChannelListContainer,ChannelContainer, Auth} from './components';
+
+const apiKey = 'rumgbj45v39p';
+
+const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
 
 const App = () => {
+
+    if(!authToken) return <Auth />
+
     return (
-        <div>
-            <h1>Hello Mama Chat Application</h1>
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer
+                
+                />
+                <ChannelContainer
+
+                />
+
+            </Chat>
+             
         </div>
     );
 }
